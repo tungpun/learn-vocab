458a895ee5432c39c6873ef3e784c1d3804c27cb
tungpun <htung.nht@gmail.com>
2016-07-06 14:46:51 +0700

    updated

---
+++ services.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pwn import *
+import random
+
+lhost = '0.0.0.0'
+lport = 2323
+
+def wrap_word(end, vie):
+    word = {
+        'eng': end,
+        'vie': vie
+    }
+    return word
+
+def extract_lines(lines):
+    words = []
+    for line in lines:
+        eng = line.split(';')[0]
+        vie = line.split(';')[1]
+        words.append(wrap_word(eng, vie))
+    return words
+
+
+def build_database(unit):
+    filename = 'unit' + str(unit) + '.txt'
+    with open(filename, 'rb') as f:
+        lines = f.readlines()
+        words = extract_lines(lines)
+    return words
+
+
+def get_a_word(words):
+    while (True):
+        w = words[random.randint(0, len(words)-1)]
+        if len(w['vie'].strip()) >= 5:
+            return w
+
+if __name__ == '__main__':    
+    all_count = 0
+    right_count = 0
+    while (True):
+        l = listen(lport, lhost)            
+        c = l.wait_for_connection()
+        c.send("Enter unit number: ")             
+        unit  = int(c.recvline().strip())
+        words = build_database(unit)            
+        while (True):                                    
+            word = get_a_word(words)  
+            try:
+                c.send("VIE: " + word['vie'].strip() + " ?\nENG: ")
+                all_count += 1
+                answer = c.recvuntil('\n').strip()
+                if answer == word['eng']:
+                    c.sendline("Correct")
+                    right_count += 1
+                else:
+                    c.sendline("ANS: " + word['eng'].strip() + " <= Right\nIncorrect")                    
+                c.sendline("Result: " + str(right_count) + "/" + str(all_count) + "\n")
+            except Exception, e:                
+                c.close()
+                break     
