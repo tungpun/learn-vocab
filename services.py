e856a36c9f13b8a20ded7dcaa5ee9ccc00bff51c
tungpun <htung.nht@gmail.com>
2016-07-06 23:01:24 +0700

    updated

--- services.py
+++ services.py
@@ -5,7 +5,7 @@ from pwn import *
 import random
 
 lhost = '0.0.0.0'
-lport = 2323
+lport = 2006
 
 def wrap_word(end, vie):
     word = {
@@ -37,27 +37,31 @@ def get_a_word(words):
         if len(w['vie'].strip()) >= 5:
             return w
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     all_count = 0
     right_count = 0
     while (True):
-        l = listen(lport, lhost)            
+        l = listen(lport, lhost)
         c = l.wait_for_connection()
-        c.send("Enter unit number: ")             
+        c.send("Enter unit number: ")
         unit  = int(c.recvline().strip())
-        words = build_database(unit)            
-        while (True):                                    
-            word = get_a_word(words)  
+        words = build_database(unit)
+        random.shuffle(words)
+        i = 0
+        while (True):
+            #word = get_a_word(words)
+            word = words[i % len(words)]
             try:
                 c.send("VIE: " + word['vie'].strip() + " ?\nENG: ")
                 all_count += 1
                 answer = c.recvuntil('\n').strip()
                 if answer == word['eng']:
                     c.sendline("Correct")
+                    i += 1
                     right_count += 1
                 else:
-                    c.sendline("ANS: " + word['eng'].strip() + " <= Right\nIncorrect")                    
+                    c.sendline("ANS: " + word['eng'].strip() + " <= Right\nIncorrect")
                 c.sendline("Result: " + str(right_count) + "/" + str(all_count) + "\n")
-            except Exception, e:                
+            except Exception, e:
                 c.close()
-                break     
+                break            
